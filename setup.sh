ea8c0c521c19eb5cfb9679e249ddf2493793738a
suraj2830009-arch <suraj2830009@gmail.com>
2025-12-27 08:18:52 +0530

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+#!/bin/bash
+# WHP-TRACKER Setup
 clear
-echo -e "\033[1;32m[*] UPDATING WHP-TRACKER SYSTEM (By Suraj)...\033[0m"
+echo -e "\e[1;33m[*] Fixing dependencies and installing requirements...\e[0m"
 pkg update && pkg upgrade -y
-pkg install python git php curl wget clang -y
-
-# Installing OSINT dependencies
-pip install holehe requests colorama phonenumbers geopy
-
-# Setting permissions
+pkg install python git -y
+pip uninstall urllib3 requests -y
+pip install --upgrade pip
+pip install requests urllib3 colorama phonenumbers geopy holehe
 chmod +x WHP-TRACKER.py
-
-echo -e "\033[1;36m[+] Setup Updated Successfully!\033[0m"
-echo -e "\033[1;33m[!] Type 'python WHP-TRACKER.py' to launch.\033[0m"
+echo -e "\e[1;32m[+] Setup Complete! Run: python WHP-TRACKER.py\e[0m"
